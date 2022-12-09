a63025df8db485b52adc6031f2d609ef44921164
tardeganesh409 <119429459+tardeganesh409@users.noreply.github.com>
2022-12-09 23:48:56 +0530

    Add files via upload

---
+++ implicitWait.java
@@ -0,0 +1,34 @@
+
+//write a script to open actitime application and logout using implicitly();
+
+//09-12-2022
+
+package demo1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class implicitWait {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromeDriver.exe");
+	}
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://demo.actitime.com/login.do");
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@name='pwd']")).sendKeys("manager");
+		driver.findElement(By.xpath("//div[.='Login ']")).click();
+		driver.findElement(By.xpath("//a[.='Logout']")).click();
+		driver.close();
+
+
+
+	}
+
+}
