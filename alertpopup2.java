5bb3a858581a1400d2ac6f3d49e52ff63aabd0d1
tardeganesh409 <119429459+tardeganesh409@users.noreply.github.com>
2022-12-21 01:41:20 +0530

    Add files via upload

---
+++ alertpopup2.java
@@ -0,0 +1,30 @@
+
+//write a script to handle popup on flipkart app
+
+
+package qsp;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class alertpopup2 {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromeDriver.exe");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+		driver.get("https://www.flipkart.com/");
+		driver.findElement(By.cssSelector("button[class='_2KpZ6l _2doB4z']")).click();
+		Thread.sleep(5000);
+		driver.close();
+	}
+}
+
+
+	
